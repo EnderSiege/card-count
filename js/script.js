2b326dd649d00915488415e43ef393d856d631ba
Christopher J. Pigg <ndrsiege@gmail.com>
2022-04-22 12:43:28 -0400

    setting functions shuffleDeck and resetCards

--- js/script.js
+++ js/script.js
@@ -9,8 +9,49 @@ function getCard() {
     return Math.floor( ( Math.random() *  14 ) );
 }
 
+// Test a few getCard calls
+// for (let i = 0; i < 11; i++) {
+//     console.log(getCard());
+// }
 
 
-for (let i = 0; i < 11; i++) {
-    console.log(getCard());
+function shuffleDeck() {
+    // Inform player the deck is being shuffled
+    console.log("Shuffling deck");
+
+    // Reset the number of cards in the deck
+    resetCards();
+
+    // Perform the shuffle
+
+}
+
+function resetCards() {
+    // Reset the number of cards in the deck
+    let 
+        aces = 4,
+        ones = 4,
+        twos = 4,
+        threes = 4,
+        fours = 4,
+        fives = 4,
+        sixes = 4,
+        sevens = 4,
+        eights = 4,
+        nines = 4,
+        tens = 4,
+        jacks = 4,
+        queens = 4,
+        kings = 4;
+}
+
+function shuffleCards() {
+    // Set deck array to empty
+    let deck = [];
+
+    // Establish a shuffled deck by placing all card values in the deck array
+    for (let i = 0; i < 52; i++) {
+        let card =  Math.floor( ( Math.random() *  14 ) );
+        
+    }
 }
