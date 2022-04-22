dfd4639e6a248e0adacf3e5c37cde11dac22370d
Christopher J. Pigg <ndrsiege@gmail.com>
2022-04-22 13:20:04 -0400

    testing basic shuffleDeck function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+// Deck array starts empty
+let deck = [];
+
 // Get a card at random
 // For now, it's working on an infinitely-sized deck
 // 1 through 10 is the same as the card face
@@ -9,51 +12,52 @@ function getCard() {
     return Math.floor( ( Math.random() *  14 ) );
 }
 
-// Test a few getCard calls
-// for (let i = 0; i < 11; i++) {
-//     console.log(getCard());
-// }
-
-
-function shuffleDeck() {
+// Shuffle the deck array
+function shuffleDeck(array) {
     // Inform player the deck is being shuffled
     console.log("Shuffling deck");
 
     // Reset the number of cards in the deck
-    resetCards();
+    array = resetCards(array);
 
     // Perform the shuffle
+    array = shuffleCards(array);
 
+    return array;
 }
 
-function resetCards() {
-    // Reset the number of cards in the deck
-    let 
-        aces = 4,
-        ones = 4,
-        twos = 4,
-        threes = 4,
-        fours = 4,
-        fives = 4,
-        sixes = 4,
-        sevens = 4,
-        eights = 4,
-        nines = 4,
-        tens = 4,
-        jacks = 4,
-        queens = 4,
-        kings = 4;
+// Reset the cards in the deck
+function resetCards(array) {
+    array = ["AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH",
+        "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD",
+        "AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC",
+        "AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS"];
+
+    return array;
 }
 
-function shuffleCards() {
-    // Set deck array to empty
-    let deck = [];
-
-    // Establish a shuffled deck by placing all card values in the deck array
-    for (let i = 0; i < 52; i++) {
-        let card =  Math.floor( ( Math.random() *  14 ) );
-
+// Fisher-Yates Shuffle
+// https://bost.ocks.org/mike/shuffle/
+function shuffleCards(array) {
+    var m = array.length, t, i;
+  
+    // While there remain elements to shuffle…
+    while (m) {
+  
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+  
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
     }
-
-    // I can already tell this is not going to work efficiently.. 
+  
+    return array;
 }
+
+console.log(deck);
+
+let shuffled = shuffleDeck(deck);
+
+console.log(shuffled);
